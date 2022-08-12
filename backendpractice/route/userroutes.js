963d593f11038a64c2fe3f81f0eaf04f489c3a78
jay9717 <110017909+jay9717@users.noreply.github.com>
2022-08-12 08:37:38 +0530

    Delete userroutes.js

--- backendpractice/route/userroutes.js
+++
@@ -1,6 +0,0 @@
-const express=require("express")
-const userrouter=express.Router()
-const usercontroller=require("../controller/usercontroller")
-userrouter.post("/create",usercontroller.register)
-userrouter.post("/login",usercontroller.login)
-module.exports=userrouter
